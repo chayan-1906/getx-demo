af2720158381f28f4db02bfaa185e32590fc110f
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-09-19 15:46:21 +0530

    #9 Separating UI Code and business logic

---
+++ lib/custom_getx_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:getx_demo/student.dart';
+
+class CustomGetXController extends GetxController {
+  // var student = Student();
+
+  // void convertToUpperCase() {
+  //   student.name.value = student.name.value.toUpperCase();
+  // }
+  var student = Student(name: 'PDas', age: 22).obs;
+
+  void convertToUpperCase() {
+    student.update((std) {
+      std?.name = std.name.toString().toUpperCase();
+    });
+  }
+}
--- lib/student_screen.dart
+++ lib/student_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_demo/custom_getx_controller.dart';
 
 import 'student.dart';
 
@@ -13,7 +14,8 @@ class StudentScreen extends StatefulWidget {
 }
 
 class _StudentScreenState extends State<StudentScreen> {
-  var student = Student(name: 'Padmanabha', age: 22).obs;
+  // var student = Student(name: 'Padmanabha', age: 22).obs;
+  CustomGetXController customGetXController = Get.put(CustomGetXController());
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,7 @@ class _StudentScreenState extends State<StudentScreen> {
             Obx(
               () => Text(
                 // 'Name is ${student.name.value}',
-                'Name is ${student.value.name}',
+                'Name is ${customGetXController.student.value.name}',
                 style: const TextStyle(fontSize: 25.0),
               ),
             ),
@@ -34,9 +36,12 @@ class _StudentScreenState extends State<StudentScreen> {
             ElevatedButton(
               onPressed: () {
                 // student.name.value = student.name.value.toUpperCase();
-                student.update((std) {
-                  std?.name = std.name.toString().toUpperCase();
-                });
+
+                // student.update((std) {
+                //   std?.name = std.name.toString().toUpperCase();
+                // });
+
+                customGetXController.convertToUpperCase();
               },
               child: const Text('Upper'),
             ),
